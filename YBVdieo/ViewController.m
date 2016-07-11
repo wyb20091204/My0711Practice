d360a612a4d6c9609f59ef730b0261b8d155d655
Yibo <wyb20091204@163.com>
2016-07-11 14:41:58 +0800

    no message

--- YBVdieo/ViewController.m
+++ YBVdieo/ViewController.m
@@ -11,6 +11,7 @@
 #define kScreenWidth [UIScreen mainScreen].bounds.size.width
 #define kScreenHeight [UIScreen mainScreen].bounds.size.height
 #define kMP4_URL @"http://dd.yinyuetai.com/uploads/videos/common/9CB00154421D3966D6A52374737DEEDF.mp4?sc=ba429cd48fedf72c&br=405&rd=Android"
+#define MM_URL @"http://pl.youku.com/playlist/m3u8?vid=409411351&ts=1468219244&ctype=12&token=0929&keyframe=1&sid=146821924476512b8fb97&ev=1&type=mp4&ep=cyaSEkuMX8wE4STZjT8bNSS0Jn4IXPoK%2FxuEgNFnAdQkQeq2&oip=2043217862"
 
 @interface ViewController ()
 
@@ -23,7 +24,7 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	
-	YBPlayerView *ybPlayerView = [[YBPlayerView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, 300) URLString:kMP4_URL];
+	YBPlayerView *ybPlayerView = [[YBPlayerView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, 300) URLString:MM_URL];
 	ybPlayerView.center = CGPointMake(kScreenWidth / 2, 200);
 	[self.view addSubview:ybPlayerView];
 	
